d96e7a5fd9c7c9892c9fe570534e3f68cdf5e6d8
darcyBeckman <darcybeckman15@gmail.com>
2020-08-27 14:44:58 +1000

    complete subjet_reader

--- prac04/subject_reader.py
+++ prac04/subject_reader.py
@@ -8,22 +8,42 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # parts = line.split(',')  # Separate the data into its parts
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        # parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
+        # print("----------")
+        lists = list(line.split(","))
+        data.append(lists)
     input_file.close()
+    return data
+
+
+def display_data(data):
+    """Displays data in a list."""
+    longest_name = get_longest_name(data)
+    for i in data:
+        print(f"{i[0]} is taught by {i[1]:<{longest_name}} and has {i[2]} students")
+
+
+def get_longest_name(data):
+    """Gets the longest name in a list of lists."""
+    longest_name = 0
+    for i in data:
+        if len(i[1]) > longest_name:
+            longest_name = len(i[1])
+    return longest_name
 
 
 main()
