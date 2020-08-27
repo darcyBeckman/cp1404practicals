955140f73e89130f2df240db3b7d736f61c19d51
darcyBeckman <darcybeckman15@gmail.com>
2020-08-27 13:57:03 +1000

    add list_warmup.py

---
+++ prac04/lists_warmup.py
@@ -0,0 +1,21 @@
+"""
+numbers[0] - 3
+numbers[-1] - 2
+numbers[3] - 1
+numbers[:-1] - 3 1 4 1 5 9
+numbers[3:4] - 1
+5 in numbers - True
+7 in numbers - False
+"3" in numbers - False
+numbers + [6, 5, 3] - 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+for n, i in enumerate(numbers):
+    if i == numbers[0]:
+        numbers[n] = "10"
+    if i == numbers[-1]:
+        numbers[n] = 1
+print(numbers)
+print(numbers[2:])
+print(9 in numbers)
